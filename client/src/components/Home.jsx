a629af49b1dc87d9a8d4773ada4a93540d63c1e7
almuzn-saif44 <16j2236390@utas.edu.om>
2026-05-19 12:58:03 -0700

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
   const [shop, setShop] = useState(null);
 
   useEffect(() => {
-    fetch("https://giftmapu-client-ugdx.onrender.com/shops")
+    fetch("https://giftmapu-server.onrender.com/shops")
       .then(res => res.json())
       .then(data => {
         setShop(data[0]);    
