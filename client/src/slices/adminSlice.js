9be50ba87c36309e3ff5580c4dfe62424ff7ed09
almuzn-saif44 <16j2236390@utas.edu.om>
2026-05-19 12:59:50 -0700

    Update adminSlice.js

--- client/src/slices/adminSlice.js
+++ client/src/slices/adminSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const BASE_URL = "https://giftmapu-client-ugdx.onrender.com";
+const BASE_URL = "https://giftmapu-server.onrender.com";
 
    
 export const getAdminShops = createAsyncThunk(
